969fcf73bc7a54a90e658f8adfa03e6f44823895
patjawat <patjawat@gmail.com>
2020-05-15 20:40:43 +0700

    update

--- yii2basic/config/web.php
+++ yii2basic/config/web.php
@@ -11,7 +11,24 @@ $config = [
         '@bower' => '@vendor/bower-asset',
         '@npm'   => '@vendor/npm-asset',
     ],
+    'modules' => [
+        // 'rbac' => 'dektrium\rbac\RbacWebModule',
+        'rbac' => [
+            'class' => 'justcoded\yii2\rbac\Module'
+        ],
+        'user' => [
+            'class' => 'dektrium\user\Module',
+        ],
+    ],
     'components' => [
+        // 'authManager' => [
+        //     'class' => 'yii\rbac\DbManager',
+        //     'defaultRoles' => ['guest', 'user'],
+        // ],
+        'authManager' => [
+            'class' => 'justcoded\yii2\rbac\components\DbManager',
+            //'class' => 'justcoded\yii2\rbac\components\PhpManager',
+        ],
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'AEmzEOZZ7M7tuDLnD5gMg1qIPWYJ12j2',
@@ -43,6 +60,7 @@ $config = [
             ],
         ],
         'db' => $db,
+
         /*
         'urlManager' => [
             'enablePrettyUrl' => true,
@@ -52,6 +70,27 @@ $config = [
         ],
         */
     ],
+    'container' => [
+        'definitions' => [
+            // you can create your own GrivView to customize all options for main roles and permissions lists.
+            'justcoded\yii2\rbac\widgets\RbacGridView' => [
+                'class' => \app\modules\admin\widgets\RbacGridView::class,
+            ],
+            // this will replace bootstrap3 ActiveForm with bootstrap4 ActiveForm.
+            'justcoded\yii2\rbac\widgets\RbacActiveForm' => [
+                'class' => \yii\bootstrap4\ActiveForm::class,
+            ],
+        ],
+    ],
+    'as routeAccess' => [
+		'class' => 'justcoded\yii2\rbac\filters\RouteAccessControl',
+		'allowActions' => [
+            // '*'
+            'site/*',
+            'rbac/*'
+		],
+		'allowRegexp' => '/(gii)/i', // optional
+	],
     'params' => $params,
 ];
 
