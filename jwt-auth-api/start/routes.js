b441145b2b94487410b75a3d9cf02e5ec09cec8f
ปัจวัฒน์ ศรีบุญเรือง <patjawat@Macintosh.local>
2020-05-18 20:02:46 +0700

    delete

--- jwt-auth-api/start/routes.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-const Route = use('Route')
-
-Route.group(() => {
-  Route.post('login', 'Auth/AuthenticationController.login')
-  Route.post('register', 'Auth/AuthenticationController.register')
-  Route.get('me', 'Auth/AuthenticationController.me').middleware(['auth'])
-}).prefix('api')
